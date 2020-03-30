d06bb71579a50524d3a132ddb3fd8b73e2820f02
saber <639878266@qq.com>
2020-03-30 22:08:35 +0800

    加了一些eslint规则

--- README.md
+++ README.md
@@ -22,3 +22,40 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+1.设置接口参数校验规则
+方式1--------------
+定义一个JSON格式
+例如
+validRules:[
+    {
+
+        field1:
+        [
+        { required: true, message: '缺少field1参数'},
+        { min: 5, max:10,message: '长度必须满足5-10个字符'},
+        { regex: '^\d{4}-\d{1,2}-\d{1,2}',message: '日期格式不正确'}
+        ],
+        field2:
+        [
+        { required: true, message: '缺少field2参数'},
+        { min: 5, max:10,message: '长度必须满足5-10个字符'},
+        { regex: '(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)',message: '身份证号格式不正确'}
+        ],
+
+    }
+
+]
+
+在调用接口时读取以上字段配置数据，遍历参数是否匹配以上规则，是就放行，否则异常
+
+如果业务系统和代码生成器是相互分离的，专门写个接口为这些配置的字段JSON生成一个sql脚本，存入到数据库表中。
+然后每个生成的接口都有这样一个函数去数据库读取数据并校验
+
+方式2----------------
+使用springboot的@valid注解中的@Pattern指定验证规则
+代码则使用反射将前端配置的规则动态传递给注解
+这个就复杂点
+
+
+
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,11 +1,70 @@
 <template>
   <div class="login_container">
-    <div class="login_box"></div>
+    <div class="login_box">
+      <div class="avatart_box">
+        <img src="../assets/logo.png"
+             alt />
+      </div>
+      <!--登录表单区域-->
+      <el-form class="login_form"
+               :model="loginForm"
+               :rules="loginFormRules"
+               ref="loginFormRef">
+        <!--账号-->
+        <el-form-item prop="username">
+          <el-input prefix-icon="el-icon-user-solid"
+                    v-model="loginForm.username">
+          </el-input>
+        </el-form-item>
+        <!--密码-->
+        <el-form-item prop="password">
+          <el-input prefix-icon="el-icon-s-goods"
+                    v-model="loginForm.password"
+                    type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary"
+                     @click="login">登录</el-button>
+          <el-button type="info"
+                     @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录账号', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(validate => {
+        console.log(validate);
+      })
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -15,12 +74,41 @@ export default {}
 }
 .login_box {
   background: #fff;
-  height: 450px;
-  width: 600px;
+  height: 300px;
+  width: 450px;
   border-radius: 5px;
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
+}
+.avatart_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #fff;
+  img {
+    height: 100%;
+    width: 100%;
+    border-radius: 50%;
+    background: #eee;
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
